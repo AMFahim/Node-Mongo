c1923058020fe34597936d2e3811b1db6e6554a7
AMFahim <amfahim2019@gmail.com>
2021-06-09 23:14:38 +0600

    dynamic api, api parameter, access params, access query

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+
+const users = ['Sogir Ali', 'Mogir Ali', 'Togir Ali', 'Bogir Ali', 'Nodir Ali'];
 
 app.get('/', (req, res) => {
   const fruit = {
@@ -12,6 +14,12 @@ app.get('/', (req, res) => {
 app.get('/fruit/bangi', (req, res) => {
   res.send({fruit:'bangi', quantity:34, price:50})
 })
+app.get('/users/:id'), (req, res) => {
+//  console.log(req.params.id)
+ const id = req.params.id;
+//  console.log(req.query.sort);
+ const name= users[id];
+  res.send(name);
+}
 
-
-app.listen (port, () => console.log("listing port 3000"));
+app.listen (port, () => console.log("listing port 4000"));
